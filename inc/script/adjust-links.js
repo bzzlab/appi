90e458ee64dd31358d2fe048f05dfc26b6c0fcf9
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2019-01-27 22:51:02 +0100

    add adjustment for video source Important: source-element cannot be adjusted. Therefore <video ... src=".."> has been used. See https://stackoverflow.com/questions/5235145/changing-source-on-html5-video-tag

--- inc/script/adjust-links.js
+++ inc/script/adjust-links.js
@@ -13,11 +13,11 @@ function filterImages(src){
 //set correct path
 function setPath(obj,attribute,src){
     //src path should be changed with the lp (Lehrperson) key
-    var lp = $("#lp_key").val();
+    let lp = $("#lp_key").val();
     if (lp == "") {
         console.log("lp is empty!");
     }
-    var sem = $("#sem_key").val();
+    let sem = $("#sem_key").val();
     if (sem == "") {
         console.log("sem is empty!");
     } else {
@@ -26,10 +26,12 @@ function setPath(obj,attribute,src){
     }
 }
 
+
 $(document).ready(function() {
-    var images = $("img");
+    //select all <src> in <img> and change href accordingly
+    let images = $("img");
     $.each(images, function() {
-        var path = $(this).attr("src");
+        let path = $(this).attr("src");
         //Filter: check if specific images' paths shouldn't be changed
         if (filterImages(path) === false) {
             //if src does not contain the path-part data, then ...
@@ -39,25 +41,63 @@ $(document).ready(function() {
         }
     });
 
+
     //select all anchor <a> in <figcaption> and change href accordingly
     images = $("figcaption a");
     $.each(images, function() {
-        var attributeType = "href";
-        var path = $(this).attr(attributeType);
+        let attributeType = "href";
+        let path = $(this).attr(attributeType);
         if (!path.includes("data")) {
             setPath($(this),attributeType,path);
         }
     });
 
-    //select all anchor <a> in <figcaption> and change href accordingly
-    // var docs = $("a");
-    // $.each(docs, function() {
-    //     var attributeType = "href";
-    //     var path = $(this).attr(attributeType);
-    //     if (!path.includes("data") && path.includes(".pdf")) {
-    //         //http://digipiph.com/blog/jquery-each-function-continue-or-break
-    //         setPath($(this),attributeType,path);
-    //     }
-    // });
-    //console.clear();
+    //select all source <source> in <video> and change src-attribute accordingly
+    images = $("video");
+    $.each(images, function() {
+        let attributeType = "src";
+        let path = $(this).attr(attributeType);
+        if (path!=null){
+            if (!path.includes("data")) {
+                setPath($(this),attributeType,path);
+            }
+        }
+    });
 });
+
+
+
+/*
+Neither this way nor the way above adjusts the path of the video > source
+Maybe by creating a video-player element with handlebars or javascript might be
+a solution. For inspiration see
+https://tomelliott.com/html-5/changing-html5-video-javascript-jquery
+https://stackoverflow.com/questions/5235145/changing-source-on-html5-video-tag
+
+ */
+/*
+function getPath(src){
+    //src path should be changed with the lp (Lehrperson) key
+    let lp = $("#lp_key").val();
+    if (lp == "") {
+        console.log("lp is empty!");
+    }
+    let sem = $("#sem_key").val();
+    if (sem == "") {
+        console.log("sem is empty!");
+    } else {
+        console.log("data/"+lp+"/"+sem+"/"+src);
+        return "data/"+lp+"/"+sem+"/"+src;
+    }
+}
+
+function adjustLink() {
+    let p = document.getElementsByTagName("video source");
+    for (let i = 0; i < p.length; i++) {
+        let path = getPath(p[i].getAttribute("src"));
+        p[i].setAttribute('src',path);
+        p[i].load();
+    }
+}
+*/
+//document.addEventListener('DOMContentLoaded', adjustLink);
